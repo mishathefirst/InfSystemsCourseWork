2927b0c5d824e5279df70b8563634aa8cd579978
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-01-02 15:44:30 +0300

    Initial commit

---
+++ springtesting/src/main/java/com/infsystems/springtesting/AppController.java
@@ -0,0 +1,17 @@
+package com.infsystems.springtesting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AppController {
+
+    @RequestMapping(value = "/greeting")
+    public String applicationController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
